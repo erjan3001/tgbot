70ebb5db5be2062b89e780251fc0353a2f96a535
erjan3001 <klopano12@gmail.com>
2026-06-11 15:23:33 +0300

    index.py

---
+++ api/index.py
@@ -0,0 +1,57 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import os
+import telebot
+from telebot import types
+
+# Бот берет данные из скрытых настроек Vercel
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+CHANNEL_NAME = os.environ.get('CHANNEL_NAME', '@somnoienglish')
+MATERIAL_LINK = os.environ.get('MATERIAL_LINK')
+
+bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
+
+def is_user_subscribed(user_id):
+    try:
+        member = bot.get_chat_member(CHANNEL_NAME, user_id)
+        return member.status in ['creator', 'administrator', 'member']
+    except Exception:
+        return False
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('1. Подписаться на канал', url=f'https://t.me{CHANNEL_NAME.replace("@", "")}'))
+    markup.add(types.InlineKeyboardButton('2. Проверить подписку', callback_data='check_sub'))
+    
+    welcome_text = (
+        "Чтобы получить материалы, пожалуйста, подпишитесь на канал и вернитесь сюда после подписки. "
+        "Нажмите кнопку проверки подписки и получите ссылку на материал."
+    )
+    bot.reply_to(message, welcome_text, reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'check_sub')
+def check_sub(call):
+    user_id = call.from_user.id
+    if is_user_subscribed(user_id):
+        bot.answer_callback_query(call.id, "Спасибо за подписку!")
+        bot.send_message(call.message.chat.id, f"Ваша ссылка: {MATERIAL_LINK}")
+    else:
+        bot.answer_callback_query(call.id, "Вы еще не подписались!", show_alert=True)
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        
+        try:
+            body_json = json.loads(post_data.decode('utf-8'))
+            update = telebot.types.Update.de_json(body_json)
+            bot.process_new_updates([update])
+        except Exception:
+            pass
+            
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'ok')
